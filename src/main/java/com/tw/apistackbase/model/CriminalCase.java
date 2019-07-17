248847a27367359ffe4522d24fb7b5f1a4352608
williamye <william.ye@oocl.com>
2019-07-17 19:53:05 +0800

    [test: add should_return_null_when_call_create_an_null_case, should_return_a_case_when_call_create_a_case] [feat: implement CriminalCase, CriminalCaseRepository]

---
+++ src/main/java/com/tw/apistackbase/model/CriminalCase.java
@@ -0,0 +1,51 @@
+package com.tw.apistackbase.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class CriminalCase {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private long incidentTime;
+
+    public CriminalCase() {}
+
+    public CriminalCase(@NotNull String name, @NotNull long incidentTime) {
+        this.name = name;
+        this.incidentTime = incidentTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getIncidentTime() {
+        return incidentTime;
+    }
+
+    public void setIncidentTime(long incidentTime) {
+        this.incidentTime = incidentTime;
+    }
+}
---
+++ src/test/java/com/tw/apistackbase/repository/CriminalCaseRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.tw.apistackbase.repository;
+
+import com.tw.apistackbase.model.CriminalCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CriminalCaseRepositoryTest {
+    @Autowired
+    private CriminalCaseRepository criminalCaseRepository;
+
+    @Before
+    public void setUp() throws Exception{
+        List<CriminalCase> criminalCases = new ArrayList<>();
+        criminalCases.add(new CriminalCase("ccc", 1563161000));
+        criminalCases.add(new CriminalCase("vvv", 1563360000));
+        criminalCases.add(new CriminalCase("bbb", 1563263512));
+        criminalCaseRepository.saveAll(criminalCases);
+    }
+
+    @Test
+    public void should_return_a_case_when_call_create_a_case() {
+        CriminalCase criminalCase = new CriminalCase("a", 1563361068);
+        CriminalCase createdCriminalCase = criminalCaseRepository.save(criminalCase);
+        Assert.assertEquals(criminalCase, createdCriminalCase);
+    }
+
+    @Test
+    public void should_return_null_when_call_create_an_null_case() {
+        CriminalCase criminalCase = new CriminalCase();
+        CriminalCase createdCriminalCase = criminalCaseRepository.save(criminalCase);
+
+        Assertions.assertThrows(Exception.class, () ->{
+            criminalCaseRepository.findAll();
+        });
+    }
+
+
+}
